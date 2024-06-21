4eb294ac2a8d02d2b2a395fe2304ae7fe4939422
TahaThinks <tahaa4587@gmail.com>
2024-06-21 21:14:29 +0400

    Append the entered data into a list of DICT

--- main.py
+++ main.py
@@ -26,11 +26,18 @@ def home():
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == "POST":
-        author_name = request.form.get('author')
-        book_name = request.form.get('book')
-        book_rating = request.form.get('rating')
-        print(author_name, book_name, book_rating)
-
+        author_name = request.form.get('author').title()
+        book_name = request.form.get('book').title()
+        book_rating = int(request.form.get('rating'))
+        # print(author_name, book_name, book_rating)
+        all_books.append(
+                {
+                    "title": book_name,
+                    "author": author_name,
+                    "rating": book_rating
+                }
+            )
+        print(all_books)
     return render_template("add.html")
 
 
