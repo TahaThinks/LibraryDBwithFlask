78a712b7a9e6a9a73bf2cf3980b05fce084ef609
TahaThinks <tahaa4587@gmail.com>
2024-06-24 03:54:11 +0400

    Create a Table

--- DBSandboxPractice/main.py
+++ DBSandboxPractice/main.py
@@ -2,3 +2,10 @@ import sqlite3
 
 db = sqlite3.connect("books-collection.db")
 cursor = db.cursor()
+
+# Create a Table
+cursor.execute("CREATE TABLE books "
+               "(id INTEGER PRIMARY KEY, "
+               "title varchar(250) NOT NULL UNIQUE, "
+               "author varchar(250) NOT NULL, "
+               "rating FLOAT NOT NULL)")
