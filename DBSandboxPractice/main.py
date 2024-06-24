b5bee87d27c169d18665641bb2be7ba7f775f4cd
TahaThinks <tahaa4587@gmail.com>
2024-06-24 04:13:02 +0400

    import sql alchemy

--- DBSandboxPractice/main.py
+++ DBSandboxPractice/main.py
@@ -1,14 +1,17 @@
-import sqlite3
-
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-
-# Create a Table
+# import sqlite3
+#
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+#
+# #Create a Table
 # cursor.execute("CREATE TABLE books "
 #                "(id INTEGER PRIMARY KEY, "
 #                "title varchar(250) NOT NULL UNIQUE, "
 #                "author varchar(250) NOT NULL, "
 #                "rating FLOAT NOT NULL)")
-
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+import sqlalchemy
+from flask import request, render_template, Flask
+import flask_sqlalchemy
